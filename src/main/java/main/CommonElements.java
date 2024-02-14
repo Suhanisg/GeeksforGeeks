a2733611d4cd8057bb777cc5589c2b390b042189
Suhanisg <suhugoyal@gmail.com>
2024-02-14 18:54:04 +0530

    CommonElements

---
+++ src/main/java/main/CommonElements.java
@@ -0,0 +1,36 @@
+package main;
+
+public class CommonElements {
+    //solution
+    class Solution
+    {
+        ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            // code here
+            int i=0,j=0,k=0;
+            ArrayList<Integer>res=new ArrayList<>();
+            while(i<n1 && j<n2 && k<n3){
+                if(A[i]==B[j] && B[j]==C[k]){
+                    if(res.contains(A[i])){
+                        i++;j++;k++;
+                        continue;
+                    }else{
+                        res.add(A[i]);
+                        i++;j++;k++;
+                        continue;
+                    }
+                }
+                else if(A[i]<B[j]){
+                    i++;
+                }
+                else if(B[j]<C[k]){
+                    j++;
+                }
+                else{
+                    k++;
+                }
+            }
+            return res;
+        }
+    }
+}
