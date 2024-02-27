5e8258dbc31dd86fac7d68ef03ff4a53e06195b9
Suhanisg <suhugoyal@gmail.com>
2024-02-27 23:49:13 +0530

    TicketCounter

---
+++ src/main/java/main/TicketCounter.java
@@ -0,0 +1,47 @@
+package main;
+
+public class TicketCounter {
+    class Solution {
+        public static int distributeTicket(int N,int K)
+        {
+            if(K==N){
+                return N;
+            }
+            if(N/K>=2){
+                int grps=N/K;
+                if(grps%2==0){
+                    if(N%K==0){
+                        int pairs=grps/2;
+                        return pairs*K+1;
+
+                    }else
+                    {
+                        int pairs=grps/2;
+                        return N-pairs*K;
+
+                    }
+
+                }else
+                {
+                    if(N%K==0){
+                        int pairs=(grps+1)/2;
+                        return pairs*K;
+
+                    }else
+                    {
+                        int pairs=(grps+1)/2;
+                        return pairs*K+1;
+
+                    }
+
+                }
+
+            }else
+            {
+                return K+1;
+            }
+
+        }
+
+    }
+}
