b42dfbf36211fc37471492f73c79943d6e49e002
Suhanisg <suhugoyal@gmail.com>
2024-02-28 23:40:38 +0530

    TwoSwaps

---
+++ src/main/java/main/TwoSwaps.java
@@ -0,0 +1,48 @@
+package main;
+
+public class TwoSwaps {
+    class Solution
+    {
+        boolean checkSorted(int N, int A[])
+        {
+            // code here
+            int count = findUnsortedCount(A);
+
+            if(count == 0 || count == 3) {
+                return true;
+            }
+
+            if(count == 4) {
+                // Swap twice and check whether the array is sorted.
+                swapOne(A);
+                swapOne(A);
+                return findUnsortedCount(A) == 0 ? true : false;
+            }
+
+            return false;
+        }
+
+        int findUnsortedCount(int[] array) {
+            int count = 0;
+            for(int i = 0; i < array.length; i++) {
+                if(array[i] != i + 1) {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        void swapOne(int[] array) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] != i + 1) {
+                    int temp = array[i];
+                    array[i] = array[temp - 1];
+                    array[temp - 1] = temp;
+
+                    break;
+                }
+            }
+        }
+    }
+}
