f18d980a89177f40096fbf28fcbab7f10fa0a54f
Suhanisg <suhugoyal@gmail.com>
2024-03-02 23:59:29 +0530

    Choice

---
+++ src/main/java/main/Choice.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Choice {
+    class Solution {
+
+        int [] arrayForm(int a, int b, int c) {
+            return new int[] {a, b, c};
+        }
+
+        String stringForm(int a, int b, int c) {
+            return String.format("%s%s%s", a,b,c);
+        }
+    }
+}
