79b8ccfcf8321298060d0c222f3dc298a4ff1cdb
Suhanisg <suhugoyal@gmail.com>
2024-03-06 20:38:15 +0530

    MinimumDistanceBetweenTwoNumbers

---
+++ src/main/java/main/MinimumDistanceBetweenTwoNumbers.java
@@ -0,0 +1,27 @@
+package main;
+
+public class MinimumDistanceBetweenTwoNumbers {
+    class Solution {
+        int minDist(int a[], int n, int x, int y) {
+            // code here
+            int minDistance = Integer.MAX_VALUE;
+            int lastIndexX = -1;
+            int lastIndexY = -1;
+            for (int i = 0; i < n; i++) {
+                if (a[i] == x) {
+                    if (lastIndexY != -1) {
+                        minDistance = Math.min(minDistance, i - lastIndexY);
+                    }
+                    lastIndexX = i;
+                } else if (a[i] == y) {
+                    if (lastIndexX != -1) {
+                        minDistance = Math.min(minDistance, i - lastIndexX);
+                    }
+                    lastIndexY = i;
+                }
+            }
+
+            return (minDistance == Integer.MAX_VALUE) ? -1 : minDistance;
+        }
+    }
+}
