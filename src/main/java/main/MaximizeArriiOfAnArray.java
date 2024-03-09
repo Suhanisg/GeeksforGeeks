0bf5f4196161c288d3f83e26fe471602835745e3
Suhanisg <suhugoyal@gmail.com>
2024-03-09 21:11:28 +0530

    MaximizeArriiOfAnArray

---
+++ src/main/java/main/MaximizeArriiOfAnArray.java
@@ -0,0 +1,22 @@
+package main;
+
+public class MaximizeArriiOfAnArray {
+
+    class Solution{
+
+        int Maximize(int arr[], int n)
+        {
+            // Complete the function
+            long sum = 0;
+            long MOD = 1000000007;
+            Arrays.sort(arr);
+            for(int i = 0; i< arr.length;i++){
+                sum = (sum+(long)arr[i]*i)%MOD ;
+
+            }
+            return (int) sum;
+
+        }
+    }
+
+}
