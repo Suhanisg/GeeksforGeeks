f3562193506868c44a41ecedcca883b038920d2e
Suhanisg <suhugoyal@gmail.com>
2024-03-14 23:45:05 +0530

    SortALinkedList

---
+++ src/main/java/main/SortALinkedList.java
@@ -0,0 +1,33 @@
+package main;
+
+public class SortALinkedList {
+    class Solution
+    {
+        //Function to sort the given linked list using Merge Sort.
+        static Node mergeSort(Node head)
+        {
+            // add your code here
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            Node temp = head;
+
+            while(temp != null)
+            {
+                arr.add(temp.data);
+                temp = temp.next;
+            }
+            Collections.sort(arr);
+
+            Node dummy = new Node(0);
+            Node curr = dummy;
+
+            for(int i = 0; i < arr.size(); i++)
+            {
+                Node node = new Node(arr.get(i));
+                curr.next = node;
+                curr = node;
+            }
+            return dummy.next;
+        }
+    }
+}
