39a2795fd85d64d8d75b2f8f14717082c17dcf41
Suhanisg <suhugoyal@gmail.com>
2024-03-21 22:40:31 +0530

    SwapAndMaximize

---
+++ src/main/java/main/SwapAndMaximize.java
@@ -0,0 +1,23 @@
+package main;
+
+public class SwapAndMaximize {
+    class GFG
+    {
+        long maxSum(long arr[] ,int n)
+        {
+            Arrays.sort(arr);
+
+            int start = 0;
+            int end = n-1;
+
+            long sum = 0;
+            while (start<end) {
+                sum += Math.abs(arr[start] - arr[end]);
+                start++;
+                end--;
+            }
+
+            return 2*sum;
+        }
+    }
+}
