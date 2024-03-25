8219732751e97d607c778b77ebdad27fd29b3856
Suhanisg <suhugoyal@gmail.com>
2024-03-25 14:36:39 +0530

    MakeArrayElementsEqual

---
+++ src/main/java/main/MakeArrayElementsEqual.java
@@ -0,0 +1,11 @@
+package main;
+
+public class MakeArrayElementsEqual {
+    class Solution {
+        public long minOperations(int N) {
+            // Code here
+            if(N%2==0)return (long)N*(long)N/4;
+            else return (long)N*((long)N/2) - ((long)N/2)*((long)N/2);
+        }
+    }
+}
