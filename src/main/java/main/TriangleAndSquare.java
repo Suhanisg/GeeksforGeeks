16b6c77067d1544699aa64d6557a9f9ec5d09bd5
Suhanisg <suhugoyal@gmail.com>
2024-03-30 19:30:44 +0530

    TriangleAndSquare

---
+++ src/main/java/main/TriangleAndSquare.java
@@ -0,0 +1,11 @@
+package main;
+
+public class TriangleAndSquare {
+    class Solution{
+        int countSquare(int B){
+            //Write your code here
+            B/=2;
+            return (B)*(B-1)/2;
+        }
+    }
+}
