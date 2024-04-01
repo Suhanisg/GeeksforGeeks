215a799f4d8bf112ac7929acd574fe86ab136acd
Suhanisg <suhugoyal@gmail.com>
2024-04-01 19:18:17 +0530

    SmallestSumContiguousSubarray

---
+++ src/main/java/main/SmallestSumContiguousSubarray.java
@@ -0,0 +1,19 @@
+package main;
+
+public class SmallestSumContiguousSubarray {
+    class Solution
+    {
+        static int smallestSumSubarray(int a[], int size)
+        {
+            // your code here
+            int min = Integer.MAX_VALUE;
+            int sum = 0;
+            for(int i=0; i<a.length;i++){
+                sum = Math.min(a[i], sum+a[i]);
+                min = Math.min(min, sum);
+            }
+            return min;
+
+        }
+    }
+}
