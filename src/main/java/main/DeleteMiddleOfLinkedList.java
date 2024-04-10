08e2b7da5417d41aabe6cae96dc6c48ac369cc66
Suhanisg <suhugoyal@gmail.com>
2024-04-10 20:32:45 +0530

    DeleteMiddleOfLinkedList

---
+++ src/main/java/main/DeleteMiddleOfLinkedList.java
@@ -0,0 +1,26 @@
+package main;
+
+public class DeleteMiddleOfLinkedList {
+    class Solution {
+        Node deleteMid(Node head) {
+            // This is method only submission.
+            // You only need to complete the method.
+            if (head == null || head.next == null) {
+                return null;
+            }
+
+            Node prev = head;
+            Node slow = head;
+            Node fast = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                prev = slow;
+                slow = slow.next;
+            }
+
+            prev.next = slow.next;
+
+            return head;
+        }
+    }
+}
