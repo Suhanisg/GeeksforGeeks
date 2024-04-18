0ffd556fd03d017874f0fa3861f8c975788cb1f1
Suhanisg <suhugoyal@gmail.com>
2024-04-18 23:52:58 +0530

    JavaCollectionsSet7LinkedList

---
+++ src/main/java/main/JavaCollectionsSet7LinkedList.java
@@ -0,0 +1,15 @@
+package main;
+
+public class JavaCollectionsSet7LinkedList {
+    class GfG
+    {
+        public void deleteNode(Node node) {
+            //Add your code here
+            if(node.next==null){
+                return;
+            }
+            node.data=node.next.data;
+            node.next=node.next.next;
+        }
+    }
+}
