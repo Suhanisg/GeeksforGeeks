8d3e42edfe5dbfad8973b87d848e984e1b953515
Suhanisg <suhugoyal@gmail.com>
2024-04-23 23:47:25 +0530

    ReverseALinkedListInGroupsOfGivenSize

---
+++ src/main/java/main/ReverseALinkedListInGroupsOfGivenSize.java
@@ -0,0 +1,36 @@
+package main;
+
+public class ReverseALinkedListInGroupsOfGivenSize {
+    class Solution
+    {
+        public static Node reverse(Node head, int k)
+        {
+            //Your code here
+            if (head == null) {
+                return null;
+            }
+
+            Node prev = null;
+            Node current = head;
+            Node next = null;
+            int count = 0;
+
+
+            while (current != null && count < k) {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+                count++;
+            }
+
+
+            if (next != null) {
+                head.next = reverse(next, k);
+            }
+
+            return prev;
+        }
+    }
+
+}
