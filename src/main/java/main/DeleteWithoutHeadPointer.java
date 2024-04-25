bff000bc7176534de57b0fda27ab74e97cb8e16c
Suhanisg <suhugoyal@gmail.com>
2024-04-25 06:30:43 +0530

    DeleteWithoutHeadPointer

---
+++ src/main/java/main/DeleteWithoutHeadPointer.java
@@ -0,0 +1,13 @@
+package main;
+
+public class DeleteWithoutHeadPointer {
+    class Solution {
+        void deleteNode(Node del_node)
+        {
+
+
+            del_node.data = del_node.next.data;
+            del_node.next = del_node.next.next;
+        }
+    }
+}
