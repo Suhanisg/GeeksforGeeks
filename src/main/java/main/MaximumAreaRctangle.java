e8dc94d685a420648df42b6c0db640b1cfe9bf6a
Suhanisg <suhugoyal@gmail.com>
2024-04-26 21:49:25 +0530

    MaximumAreaRctangle

---
+++ src/main/java/main/MaximumAreaRctangle.java
@@ -0,0 +1,23 @@
+package main;
+
+public class MaximumAreaRctangle {
+    class maxArea{
+
+        static int calculate_Area(Rectangle arr[]){
+
+            // Your code here
+            int max=0;
+            int area=0;
+            for(int i=0;i<arr.length;i++){
+                area = arr[i].length * arr[i].breadth;
+
+
+                if(area>max){
+                    max=area;
+                }
+
+            }
+            return max;
+        }
+    }
+}
