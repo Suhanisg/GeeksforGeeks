4fae567dba7150ae0f3834de893dc9e69ba22938
Suhanisg <suhugoyal@gmail.com>
2024-05-12 23:24:03 +0530

    TomAndJerry

---
+++ src/main/java/main/TomAndJerry.java
@@ -0,0 +1,10 @@
+package main;
+
+public class TomAndJerry {
+    class Solution {
+        static int numsGame(int N) {
+            // code here
+            return (N%2==0)?1:0;
+        }
+    };
+}
