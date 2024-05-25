27fea9fd4f305dfc8ee9aa0b0e46298f95e2faea
Suhanisg <suhugoyal@gmail.com>
2024-05-25 23:00:58 +0530

    RopeCutting

---
+++ src/main/java/main/RopeCutting.java
@@ -0,0 +1,25 @@
+package main;
+
+public class RopeCutting {
+    class Solution{
+
+        // Function for finding maximum and value pair
+        public static ArrayList<Integer> RopeCutting (int arr[], int n) {
+            // Complete the Function
+            ArrayList<Integer> L1 = new ArrayList<>();
+            Arrays.sort(arr);
+
+            int i= 0;
+            int j = 1;
+
+            while(i<n && j < n){
+                if( arr[j] - arr[i] > 0 ){
+                    L1.add(n-j);
+                }
+                i++;
+                j++;
+            }
+            return L1;
+        }
+    }
+}
