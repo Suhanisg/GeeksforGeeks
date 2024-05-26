eaced1d8ba76173a2055d367da4665a5ffdfba6b
Suhanisg <suhugoyal@gmail.com>
2024-05-26 23:12:32 +0530

    zeroSumSubarrays

---
+++ src/main/java/main/zeroSumSubarrays.java
@@ -0,0 +1,34 @@
+package main;
+
+public class zeroSumSubarrays {
+    class Solution{
+        //Function to count subarrays with sum equal to 0.
+        public static long findSubarray(long[] arr ,int n)
+        {
+            HashMap<Integer, Integer> sumFreqMap = new HashMap<>();
+
+            int count = 0;
+            int sum = 0;
+
+
+            for (int i = 0; i < arr.length; i++) {
+
+                sum += arr[i];
+
+
+                if (sum == 0)
+                    count++;
+
+
+                if (sumFreqMap.containsKey(sum))
+                    count += sumFreqMap.get(sum);
+
+                sumFreqMap.put(sum, sumFreqMap.getOrDefault(sum, 0) + 1);
+            }
+
+            return count;
+        }
+    }
+
+
+}
