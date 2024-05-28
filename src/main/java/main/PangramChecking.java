7b112b59e435b0c9e89878f7cc995cf309ae49e2
Suhanisg <suhugoyal@gmail.com>
2024-05-28 22:35:26 +0530

    PangramChecking

---
+++ src/main/java/main/PangramChecking.java
@@ -0,0 +1,30 @@
+package main;
+
+public class PangramChecking {
+    class Solution
+    {
+        public static boolean checkPangram  (String s) {
+            // your code here
+            if (s.length() < 26) {
+                return false;
+            }
+            String str = s.toLowerCase();
+            HashSet<Character> set = new HashSet<>();
+            for (int i = 0; i < s.length(); i++) {
+                char ch = str.charAt(i);
+                if (ch <= 123 && ch >= 97) {
+                    set.add(ch);
+                }
+
+            }
+
+            if (set.size() == 26) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+}
