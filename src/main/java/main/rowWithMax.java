4c7fd0ffe7628ab04b53bccef4beabe753518788
Suhanisg <suhugoyal@gmail.com>
2024-06-05 19:41:09 +0530

    rowWithMax

---
+++ src/main/java/main/rowWithMax.java
@@ -0,0 +1,34 @@
+package main;
+
+public class rowWithMax {
+    class Solution {
+        int rowWithMax1s(int arr[][], int n, int m) {
+            // code here
+
+            int rowMax = 0;
+            int row = -1;
+
+            for(int i = 0; i<n; i++)
+            {
+                int oneCount = 0;
+
+                for(int j=0; j<m; j++)
+                {
+                    if(arr[i][j] == 1)
+                    {
+                        oneCount++;
+                    }
+                }
+
+                if(oneCount > rowMax)
+                {
+                    rowMax = oneCount;
+                    row = i;
+                }
+            }
+
+            return row;
+        }
+    }
+
+}
