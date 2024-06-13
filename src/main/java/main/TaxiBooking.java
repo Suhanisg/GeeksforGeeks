d9715670e231765fc94e7acb95e749f469bf7197
Suhanisg <suhugoyal@gmail.com>
2024-06-13 21:36:26 +0530

    TaxiBooking

---
+++ src/main/java/main/TaxiBooking.java
@@ -0,0 +1,19 @@
+package main;
+
+public class TaxiBooking {
+    class Solution {
+        public static int minimumTime(int N, int cur, int[] pos, int[] time) {
+            // code here
+            int min = Integer.MAX_VALUE;
+
+            for(int i = 0; i < N; i++) {
+                int currentTime = Math.abs(pos[i] - cur) * time[i];
+                if(currentTime < min) {
+                    min = currentTime;
+                }
+            }
+
+            return min;
+        }
+    }
+}
