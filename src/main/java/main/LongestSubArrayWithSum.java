6688c9d01f4d0147885140637e42689bc26b72ec
Suhanisg <suhugoyal@gmail.com>
2024-06-14 20:17:56 +0530

    LongestSubArrayWithSum

---
+++ src/main/java/main/LongestSubArrayWithSum.java
@@ -0,0 +1,28 @@
+package main;
+
+public class LongestSubArrayWithSum {
+    class Solution {
+        public static int lenOfLongSubarr (int arr[], int N, int k) {
+            int maxlen=0;
+            int sum=0;
+            HashMap<Integer,Integer>mymap=new HashMap<>();
+            mymap.put(0,-1);
+            for(int i=0;i<N;i++){
+                sum =sum+arr[i];
+                int rem=sum-k;
+                if(mymap.containsKey(rem)){
+                    int len=i-mymap.get(rem);
+                    maxlen=Math.max(maxlen,len);
+
+                }
+                if(!mymap.containsKey(sum)){
+                    mymap.put(sum,i);
+
+                }
+
+            }
+            return maxlen;
+        }
+    }
+
+}
