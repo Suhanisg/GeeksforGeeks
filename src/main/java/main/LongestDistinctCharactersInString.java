6a587a17fc4645435a65c8640d5a5165ac6f89bf
Suhanisg <suhugoyal@gmail.com>
2024-06-15 22:38:05 +0530

    LongestDistinctCharactersInString

---
+++ src/main/java/main/LongestDistinctCharactersInString.java
@@ -0,0 +1,27 @@
+package main;
+
+public class LongestDistinctCharactersInString {
+    class Solution{
+        static int longestSubstrDistinctChars(String S){
+            // code here
+            HashMap<Character, Integer> map= new HashMap<Character, Integer>();
+
+            int l = 0;
+            int r =0;
+            int n =  S.length();
+            int len = 0;
+
+            while(r < n){
+                if(map.containsKey(S.charAt(r))){
+                    l =  Math.max(map.get(S.charAt(r))+1,l);
+                }
+
+                map.put(S.charAt(r),r);
+                len = Math.max(r-l+1,len);
+                r++;
+            }
+            return len;
+
+        }
+    }
+}
