48efa78ed0941d89af16b23a403b226dd7e65e7e
Suhanisg <suhugoyal@gmail.com>
2024-06-20 22:33:00 +0530

    RemoveDuplicates

---
+++ src/main/java/main/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+package main;
+
+public class RemoveDuplicates {
+    class Solution {
+        String removeDups(String S) {
+
+            HashSet<Character>set =new HashSet<>();
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0 ; i<S.length() ; i++){
+                if(set.add(S.charAt(i))){
+                    sb.append(S.charAt(i));
+                }
+            }
+
+            return sb.toString();
+
+        }
+    }
+}
