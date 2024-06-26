e291e028be1c901c210865f1c24fb1f76d0d3d3c
Suhanisg <suhugoyal@gmail.com>
2024-06-26 22:07:57 +0530

    UnionOfTtwoSortedArrays

---
+++ src/main/java/main/UnionOfTtwoSortedArrays.java
@@ -0,0 +1,18 @@
+package main;
+
+public class UnionOfTtwoSortedArrays {
+    class Solution
+    {
+
+        public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+        {
+            Set<Integer> set=new TreeSet<>();
+            for(int num:arr1){
+                set.add(num);}
+            for(int num:arr2){
+                set.add(num);}
+
+            return new ArrayList<>(set);
+        }
+    }
+}
