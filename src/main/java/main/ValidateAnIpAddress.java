5e54cde5e18d4b5bd2d749b4a5527b7ceb0cafff
Suhanisg <suhugoyal@gmail.com>
2024-07-01 18:58:52 +0530

    ValidateAnIpAddress

---
+++ src/main/java/main/ValidateAnIpAddress.java
@@ -0,0 +1,29 @@
+package main;
+
+public class ValidateAnIpAddress {
+    class Solution {
+        public boolean isValid(String s) {
+            int count=0;
+            String arr[]=s.split("\\.");
+            if(arr.length!=4)
+                return false;
+            if(s.startsWith(".")||s.endsWith("."))
+                return false;
+            for(String str :arr){
+                if(str.length()>3||(str.length()>1&&str.charAt(0) == '0'))
+                    return false;
+                try{
+                    int val = Integer.valueOf(str);
+                    if(!(val>=0&&val<=255)){
+                        return false;
+                    }
+                }catch(NumberFormatException e){
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
+
