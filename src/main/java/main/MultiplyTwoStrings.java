4a84eda916251baba6532fe8b561b43255653bdb
Suhanisg <suhugoyal@gmail.com>
2024-07-03 21:57:19 +0530

    MultiplyTwoStrings

---
+++ src/main/java/main/MultiplyTwoStrings.java
@@ -0,0 +1,18 @@
+package main;
+
+public class MultiplyTwoStrings {
+
+    class Solution
+    {
+        public String multiplyStrings(String s1,String s2)
+        {
+            String product = "";
+            BigInteger a = new BigInteger(s1);
+            BigInteger b = new BigInteger(s2);
+            BigInteger mul = a.multiply(b);
+            product = product + mul.toString();
+            return product;
+
+        }
+    }
+}
