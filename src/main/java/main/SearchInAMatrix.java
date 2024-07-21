e222bc92a06250c0112a9ca9322d967c70b62ea2
Suhanisg <suhugoyal@gmail.com>
2024-07-21 22:14:43 +0530

    SearchInAMatrix

---
+++ src/main/java/main/SearchInAMatrix.java
@@ -0,0 +1,20 @@
+package main;
+
+public class SearchInAMatrix {
+    class Sol
+    {
+        public static int matSearch(int mat[][], int N, int M, int X)
+        {
+            int row=0;
+            int col= M-1;
+
+            while(row<N && col>=0){
+                if ( mat[row][col] == X)return 1;
+                else if( mat[row][col] < X )row++;
+                else {col--;}
+            }
+            return 0;
+
+        }
+    }
+}
