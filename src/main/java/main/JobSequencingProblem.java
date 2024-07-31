77a50e0dd56ae2f4084faa82f4e20c7c2e09e27e
Suhanisg <suhugoyal@gmail.com>
2024-07-31 21:58:27 +0530

    JobSequencingProblem

---
+++ src/main/java/main/JobSequencingProblem.java
@@ -0,0 +1,27 @@
+package main;
+
+public class JobSequencingProblem {
+
+    class Solution
+    {
+        //Function to find the maximum profit and the number of jobs done.
+        int[] JobScheduling(Job arr[], int n)
+        {
+            Arrays.sort(arr, (a,b)-> b.profit-a.profit);
+            boolean p[] = new boolean[n+1];
+            int res[] = new int[2];
+            for(Job job: arr) {
+                for(int i=job.deadline; i>=1; i--) {
+                    if(!p[i]) {
+                        p[i] = true;
+                        res[0]++;
+                        res[1] += job.profit;
+                        break;
+                    }
+                }
+            }
+            return res;
+
+        }
+    }
+}
