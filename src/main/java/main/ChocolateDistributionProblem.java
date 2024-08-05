6698456847570a35c64c9d82288369ab6e9169e9
Suhanisg <suhugoyal@gmail.com>
2024-08-05 22:21:30 +0530

    ChocolateDistributionProblem

---
+++ src/main/java/main/ChocolateDistributionProblem.java
@@ -0,0 +1,26 @@
+package main;
+
+public class ChocolateDistributionProblem {
+    class Solution
+    {
+        public long findMinDiff (ArrayList<Integer> a, int n, int m)
+        {
+            Collections.sort(a);
+
+            int i=0,j=0,count=0;
+            long min_count=Integer.MAX_VALUE;
+
+            while(j<a.size()){
+                if(j-i>=m-1){
+                    count=a.get(j)-a.get(i);
+                    min_count=Math.min(min_count,count);
+                    count=0;
+                    i++;
+                }
+                j++;
+            }
+            return min_count;
+
+        }
+
+    }
