e07c779511afa791490d8911ad06af7f4eb2b7d2
Suhanisg <suhugoyal@gmail.com>
2024-08-06 19:28:08 +0530

    SearchInARotatedArray

---
+++ src/main/java/main/SearchInARotatedArray.java
@@ -0,0 +1,14 @@
+package main;
+
+public class SearchInARotatedArray {\class Solution {
+    int search(int[] arr, int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
+}
