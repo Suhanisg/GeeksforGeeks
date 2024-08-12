aaa2be437bcf07e4573263b019d54ac73ff01bd0
Suhanisg <suhugoyal@gmail.com>
2024-08-11 22:21:50 -0300

    SquareRoot

---
+++ src/main/java/main/SquareRoot.java
@@ -0,0 +1,19 @@
+package main;
+
+public class SquareRoot {
+    class Solution {
+        long floorSqrt(long n) {
+            // Your code here
+            long a = 0 ;
+            if(n==0 || n==1){
+                a = n;
+            }
+            for(long i = 1 ; i*i<=n ; i++ ){
+                a=i;
+            }
+            return a;
+
+        }
+    }
+
+}
