c256417a5a78cd6d28bc0f6bf7276a35f7e7dd9d
Suhanisg <suhugoyal@gmail.com>
2024-08-16 22:28:16 -0300

    CountTheTriplets

---
+++ src/main/java/main/CountTheTriplets.java
@@ -0,0 +1,31 @@
+package main;
+
+public class CountTheTriplets {
+    class Solution {
+        int countTriplet(int arr[], int n) {
+            // code here
+            int count = 0;
+            Arrays.sort(arr);
+            for(int i=n-1;i>-1;i--){
+                int j = 0;
+                int k = i-1;
+                while(j<k){
+                    int sum = arr[j] + arr[k];
+                    if(sum == arr[i]){
+                        count++;
+                        j++;
+                        k--;
+                    }
+                    else if(sum > arr[i]){
+                        k--;
+                    }
+                    else{
+                        j++;
+                    }
+                }
+            }
+            return count;
+
+        }
+    }
+}
