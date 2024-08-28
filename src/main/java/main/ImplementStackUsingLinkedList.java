da366699da9d8849bd089471cbf75d7dd0db3a08
Suhanisg <suhugoyal@gmail.com>
2024-08-27 23:00:18 -0300

    ImplementStackUsingLinkedList

---
+++ src/main/java/main/ImplementStackUsingLinkedList.java
@@ -0,0 +1,32 @@
+package main;
+
+public class ImplementStackUsingLinkedList {
+    class MyStack {
+        // class StackNode {
+        //     int data;
+        //     StackNode next;
+        //     StackNode(int a) {
+        //         data = a;
+        //         next = null;
+        //     }
+        // }
+        StackNode top;
+
+        // Function to push an integer into the stack.
+        void push(int a)
+        {
+            StackNode newnode = new StackNode(a);
+            newnode.next = top;
+            top=newnode;
+        }
+        int pop()
+        {
+            if(top == null)
+                return -1;
+            StackNode temp = top;
+            top = top.next;
+            return temp.data;
+
+        }
+    }
+}
