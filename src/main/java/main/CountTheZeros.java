3005313a3eb872b557c9347dff954ee32a59a14c
Suhanisg <suhugoyal@gmail.com>
2024-09-08 21:50:45 -0300

    CountTheZeros

---
+++ src/main/java/main/CountTheZeros.java
@@ -0,0 +1,27 @@
+package main;
+
+public class CountTheZeros {
+    class Solution {
+
+        int countZeroes(int[] arr) {
+            // code here
+            int l=0;
+            int r=arr.length-1;
+            if(arr[r]==1){
+                return 0;
+            }
+            int c=0;
+            while(l<=r){
+                int mid=l+(r-l)/1;
+                if(arr[mid]==0){
+                    c++;
+                    r=mid-1;
+                }
+                if(arr[mid]==1){
+                    l=mid+1;
+                }
+            }
+            return c;
+
+        }
+    }}
