93cc63c6359199fbb712f637bc361a13f64ed0c0
Suhanisg <suhugoyal@gmail.com>
2024-09-11 23:13:24 -0300

    ImplementStrstr

---
+++ src/main/java/main/ImplementStrstr.java
@@ -0,0 +1,20 @@
+package main;
+
+public class ImplementStrstr {
+
+    class GfG
+    {
+        //Function to locate the occurrence of the string x in the string s.
+        int strstr(String s, String x)
+        {
+            // Your code here
+            int index = s.indexOf(x);  // Find the first occurrence of x in s
+
+            if(index != -1) {
+                return index; // If x is found, return the index
+            }
+
+            return -1; // If x is not found, return -1
+        }
+    }
+}
