005ee67c13215c20e71ec3a2a55449737e246a67
Suhanigo <suhani.goyal_cs22@gla.ac.in>
2024-09-23 22:24:01 -0300

    MinimumInteger

---
+++ src/main/java/main/MinimumInteger.java
@@ -0,0 +1,20 @@
+package main;
+
+public class MinimumInteger {
+    class Solution {
+        public static int minimumInteger(int N, int[] A) {
+            // code here
+            long sum=Arrays.stream(A).summaryStatistics().getSum();
+
+            int ans=Integer.MAX_VALUE;
+            for(int i=0;i<N;i++)
+            {
+                long mul=((long)N*(long)A[i]);
+                if(sum<=mul)  ans=Math.min(ans,A[i]);
+            }
+
+            return ans;
+
+        }
+    }
+}
