014dbf53bc0565b3467b59132e05f8779a69b822
Suhanigo <suhani.goyal_cs22@gla.ac.in>
2024-10-11 20:56:19 -0300

    InFirstButSecond

---
+++ src/main/java/main/InFirstButSecond.java
@@ -0,0 +1,23 @@
+package main;
+
+public class InFirstButSecond {
+
+    class Solution
+    {
+        ArrayList<Integer> findMissing(int a[], int b[], int n, int m)
+        {
+            ArrayList<Integer> res=new ArrayList<>();
+            HashSet<Integer> set=new HashSet<>();
+            for(int i:b){
+                set.add(i);
+            }
+            for(int i:a){
+                if(!set.contains(i)){
+                    res.add(i);
+                }
+            }
+            return res;
+
+        }
+    }
+}
