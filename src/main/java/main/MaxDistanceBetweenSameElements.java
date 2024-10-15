e860d98ad6fcfb51a974eab9dc9c6ceedcdf6bfd
Suhanigo <suhani.goyal_cs22@gla.ac.in>
2024-10-14 23:11:59 -0300

    MaxDistanceBetweenSameElements

---
+++ src/main/java/main/MaxDistanceBetweenSameElements.java
@@ -0,0 +1,26 @@
+package main;
+
+public class MaxDistanceBetweenSameElements {
+    class Solution {
+        public int maxDistance(int[] arr) {
+            // Code here
+            int maxDistance=0;
+            HashMap<Integer,Integer> map=new HashMap<>();
+            for(int i=0;i<arr.length;i++)
+            {
+                if(map.containsKey(arr[i]))
+                {
+                    maxDistance=Math.max(maxDistance,i-map.get(arr[i]));
+                }
+                else
+                {
+                    map.put(arr[i],i);
+                }
+            }
+            return maxDistance;
+
+        }
+
+
+    }
+}
