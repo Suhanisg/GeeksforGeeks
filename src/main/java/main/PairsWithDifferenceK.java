2e26d1f384920c8b0cacb7361661b267f3eb1adb
Suhanisg <suhugoyal@gmail.com>
2024-11-16 21:06:06 +0530

    PairsWithDifferenceK

---
+++ src/main/java/main/PairsWithDifferenceK.java
@@ -0,0 +1,20 @@
+package main;
+
+public class PairsWithDifferenceK {
+    class Solution {
+        int countPairsWithDiffK(int[] arr, int k)
+        {
+            // code here
+            int res = 0;
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for(int i=0; i < arr.length; i++) {
+                res += map.getOrDefault(arr[i] + k, 0);
+                res += map.getOrDefault(arr[i] - k, 0);
+
+                map.put(arr[i], map.getOrDefault(arr[i] , 0) + 1);
+            }
+            return res;
+
+        }
+    }
+}
